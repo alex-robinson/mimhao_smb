97bce367783d5ecf99fc790d88e6f0d05d43997c
Alexander Robinson <robinson@fis.ucm.es>
2016-04-23 13:08:58 +0200

    Cleaned up data loading routines, now can use 'load_data.r' using the RNetCDF library.

--- load_data.r
+++ load_data.r
@@ -1,34 +1,22 @@
-## Script for load the data from .nc with ncdf
-## Function get.nc made by Alex Robinson
-source("functions_ncdf.r")
+## Script to load the data from .nc with RNetCDF library
+library(RNetCDF)
+library(stringr)
 
-#dir_data<-"ice_data/Antarctica/ANT-40KM_clim"
+data_fldr = "data/ANT-40KM"
 
-patch <- c("ice_data/Antarctica/ANT-40KM_clim","mapping")
-files <- list.files(patch, full.names = TRUE)
-## files <- list.files("ice_data/Antarctica/ANT-40KM_clim", full.names = TRUE)
-whatisdir <- file.info(files)$isdir
-files <- files[!whatisdir]
-files <- files[grep("nc$",files)]
+# Determine filenames of all data files
+files = list.files(data_fldr)
 
-## files_names <- list.files("ice_data/Antarctica/ANT-40KM_clim")
-## files_names <- files_names[!whatisdir]
-## # install.packages("stringr")
-## require(stringr)
-## delstr <- c("ANT-40KM_", ".nc")
-## for (i in 1:length(delstr)){
-##     files_names <- str_replace_all(files_names, delstr[i], "")
-##     files_names <- str_replace_all(files_names, "-", "_")
-## }
-files_names <- files
-delstr <- c(patch, "ANT_40KM_", ".nc","/")
-require(stringr)
-for (i in 1:length(delstr)){    
-    files_names <- str_replace_all(files_names, delstr[i], "")
-    files_names <- str_replace_all(files_names, "-", "_")
+
+# Extract dataset names from filenames
+delstr    = c("ANT-40KM_", ".nc")
+dat_names = files
+for (i in 1:length(delstr)){
+    dat_names = str_replace_all(dat_names, delstr[i], "")
+    dat_names = str_replace_all(dat_names, "-", "_")
 }
-
-for (i in 1:length(files_names)) {
-    assign(files_names[i], get.nc(files[i]))
+for (i in 1:length(dat_names)) {
+    nc = open.nc(file.path(data_fldr,files[i]))
+    assign(dat_names[i], read.nc(nc))
+    close.nc(nc)
 }
-rm(list = c("i","whatisdir","delstr","files_names","files","patch","get.nc"))
