5ecb0ff7dbf9f5285fb60db4d96282536a81f272
Alexander Robinson <robinson@fis.ucm.es>
2016-06-29 13:29:54 +0200

    Updated script.

--- calc_rignot_table.r
+++ calc_rignot_table.r
@@ -6,18 +6,22 @@ library(openxlsx)
 
 
 # Load original Rignot data 
-tmp = read.xlsx("data/1235798tableS1.xlsx",rows=c(1,3:72),cols=c(1:14))
+rig0 = read.xlsx("data/1235798tableS1.xlsx",rows=c(1,3:72),cols=c(1:14))
 
 
 # Generate a new table, with values instead of strings in the right columns 
-rig = data.frame(name=tmp$Ice.Shelf.Name,area=tmp$Actual.area,GL=NA,SMB=NA,IF=NA,dHdt=NA,BM=NA)
+rig = data.frame(name=rig0$Ice.Shelf.Name,area=rig0$Actual.area,GL=NA,SMB=NA,IF=NA,dHdt=NA,BM=NA,
+                 GL.err=NA,SMB.err=NA,IF.err=NA,dHdt.err=NA,BM.err=NA)
 
 
 # Convert the strings to numbers in each column of interest
 nrow = dim(rig)[1]
 for (q in 1:nrow) {
 
-    rig$GL[q] = as.numeric(strsplit(tmp$Grounding.line.flux[q],"±")[[1]][1])
+    tmp = strsplit(rig0$Grounding.line.flux[q],"±")[[1]]
+    tmp1 = strsplit(tmp[2]," ")[[1]][1]
+    rig$GL[q]     = tmp[1]
+    rig$GL.err[q] = tmp1
 
 }
 
